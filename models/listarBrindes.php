7eeab6a85e0f26fa9803d857eb533d88fd487bbe
EduViva <miissjackson72@gmail.com>
2020-04-29 19:52:29 -0300

    Versão 1 pronta, já em uso

---
+++ models/listarBrindes.php
@@ -0,0 +1,31 @@
+<?php
+
+include "../controllers/dbAccess.php";
+
+$total = $_GET['total'];
+
+
+date_default_timezone_set('America/Sao_Paulo');
+$atualDate = date('M');
+
+
+$return = $db->query("SELECT COUNT(*) FROM brindes WHERE mes = '". $atualDate . "'");
+
+if($return){
+  while ($row = $return->fetch_assoc()) {
+    $mesBrindes = $row['COUNT(*)'];
+  }
+}
+
+if($mesBrindes > $total){
+
+  echo true;
+
+  //$pesquisa = $db->query("SELECT * FROM brindes WHERE mes = '" . $atualDate . "'");
+
+  //foreach ($pesquisa as $key => $row) {
+    //echo $row['finalidade'] .",". $row['proprietario'] .",". $row['inquilino'] .",". $row['valor'] .",";
+  //}
+
+}
+?>
